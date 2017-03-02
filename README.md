b3c3631aace529eb2bcf8e59f8cacd3a44c4c198
buhrmi <buhrmi@gmail.com>
2017-03-02 22:30:36 +0900

    authentication, syncing

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
-# vue-pouch
+# vue-pouch 👌👌👌
 
-Live and reactive PouchDB bindings for Vuejs with **[Mango Queries](http://docs.couchdb.org/en/2.0.0/api/database/find.html)** 👌👌👌
+PouchDB integration for Vuejs with live and reactive **[Mango Queries](http://docs.couchdb.org/en/2.0.0/api/database/find.html)**, **Remote Syncing**, and **Authentication**. It's all you need to build the next awesome PWA.
 
 ![If you have Pouch and Vue, you have Pouch and Vue](https://github.com/QurateInc/vue-pouch/blob/master/vue-pouch.png)
 
 Refer to https://github.com/nolanlawson/pouchdb-find for documentation on the query structure and a guide on how to create indexes.
 
-## Example
+## Examples
+
+### Live and reactive Mango Queries
 
 ```vue
 <template>
@@ -47,6 +49,40 @@ Refer to https://github.com/nolanlawson/pouchdb-find for documentation on the qu
 </script>
 ```
 
+### User Authentication
+
+```
+<template>
+  <div class="credentials">
+    <button v-if="$pouch.hasAuth" @click="$pouch.resetAuth()">Reset Authentication</button>
+    <button v-else @click="$pouch.useAuth('myname', 'mypassword')">Authenticate</button>
+    <button @click="$pouch.createUser('myname', 'mypassword')">Create User</button>
+    Your remote session name: {{ $pouch.session.name }}
+    <div class="error" v-if="$pouch.error">There was an error: {{ $pouch.error }}</div>
+  </div>
+</template>
+```
+
+### Remote Syncing
+
+```
+<template>
+  <div class="credentials">
+    <div class="error" v-if="$pouch.error.blog">{{ $pouch.error.blog }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  created: function() {
+    // This will set up a live sync
+    this.$pouch.sync('blog', 'remotehost/blog')
+  }
+}
+</script>
+```
+
+
 ## Installation
 
 Install via npm:
@@ -59,15 +95,48 @@ The only requirement is that `pouchdb-live-find` is installed:
     PouchDB.plugin(require('pouchdb-find'));
     PouchDB.plugin(require('pouchdb-live-find'));
     
-Then, plug `vue-pouch` into Vue:
+If you want to use remote databases (CouchDB, Cloudant, etc.), you should also install the authentication plugin:
+
+    PouchDB.plugin(require('pouchdb-authentication'));
+    
+Then, plug VuePouch into Vue:
 
     Vue.use(require('vue-pouch'), {
       pouch: PouchDB,    // optional if `PouchDB` is available on the global object
-      defaultDB: someDB  // optional, database name (string) or a PouchDB instance
     })
+
+## API
+
+When using VuePouch, don't use `new PouchDB(...)`, `db.login` or `db.sync` directly, since VuePouch needs to hook into their callbacks. Instead, use the functions provided by the `$pouch` property on your vue instance.
+
+### $pouch
+
+In theory, VuePouch can handle multiple remote servers at the same time. However,
+all user authentication and session management is done on the **first** http/https
+database that has been configured using the `$pouch.sync` method.
+
+#### Methods
+
+* `$pouch.sync(localDatabase, remoteDatabase)`: Sets up a remote database. Also, if the browser has an active session cookie, it will fetch session data (username, etc) from the remote server.
+* `$pouch.createUser(name, password)`: Create a user in the remote database and also start a new session.
+* `$pouch.useAuth(name, password)`: Set credentials to use to start a session with the remote server.
+* `$pouch.resetAuth()`: Forgets the credentials, session data and session cookie.
+
+#### Reactive Properties
+
+* `$pouch.hasAuth`: `true`, if VuePouch has credentials
+* `$pouch.authError`: Contains the authentication error, if one occured (eg. when calling useAuth, createUser, etc).
+* `$pouch.session`: Contains information about the current session with the remote database (eg. user name, roles, etc.)
+* `$pouch.errors`: A json object containing errors that occured on databases. The object key is the name of the database, the value is the error.
 
 ## Todo
 
-* [ ] Forward errors (no connection, unauthorized, etc) to vue component
-* [ ] Set up and tear down database syncing in pouch config options
-* [ ] Reactive properties that indicate a *loading*, *ready* and *error* state
+These things are on the list to be doped out at a later time.
+
+* [ ] Lazy (on-demand) attachments
+* [ ] Resumable uploads
+* [ ] Conflict handling after offline changes
+* [ ] Way to handle sync errors (no connection, auth errors, ..) in reactive way
+* [ ] Change username/password, store user meta info
+* [ ] Two-factor auth
+* [ ] Third-party auth
--- index.js
+++ index.js
@@ -2,21 +2,106 @@
   var vue = null;
   var pouch = null;
   var defaultDB = null;
+  var defaultUsername = null;
+  var defaultPassword = null;
   var databases = {};
-  var pouchdbvue = {
+  
+  var vuePouch = {
     created: function() {
       if (!vue) {
-        console.warn('[pouchdb-vue] not installed!')
+        console.warn('[vue-pouch] not installed!')
         return
       }
       var defineReactive = vue.util.defineReactive;
       var vm = this;
+      
+      var liveFinds = {};
+      
+      function fetchSession(database) {
+        if (['http','https'].indexOf(database.adapter) == -1) return;
+        database.getSession().then(function(res) {
+          database.getUser(res.userCtx.name).then(function(res) {
+            vm.$pouch.session = res;
+          }).catch(function(error) {
+            vm.$pouch.authError = error
+          });
+        })
+      }
+      function login(database) {
+        if (['http','https'].indexOf(database.adapter) == -1) return;
+        database.login(defaultUsername, defaultPassword).then(function(res) {
+          vm.$pouch.session = res;
+          database.getUser(defaultUsername).then(function(res) {
+            vm.$pouch.session = res;
+          })
+        }).catch(function(error) {
+          vm.$pouch.authError = error
+        })
+      }
+      function logout(database) {
+        database.logout()
+      }
+      
+      var $pouch = {
+        useAuth: function(username, password) {
+          defaultUsername = username;
+          defaultPassword = password;
+          for (var dbString in databases) {
+            if (!databases.hasOwnProperty(dbString)) continue;
+            login(databases[dbString]);
+          }
+          vm.$pouch.gotAuth = true;
+        },
+        createUser: function(username, password) {
+          defaultDB.signup(username, password).then(function(res) {
+            vm.$pouch.useAuth(username, password);
+          }).catch(function(error) {
+            vm.$pouch.authError = error;
+          })
+        },
+        resetAuth: function() {
+          defaultUsername = null;
+          defaultPassword = null;
+          vm.$pouch.gotAuth = false;
+          vm.$pouch.session = {}
+          if (defaultDB) defaultDB.logout()
+        },
+        sync: function(localDB, remoteDB) {
+          var pausedCount = 0;
+          if (!databases[localDB])  databases[localDB] = new pouch(localDB);
+          if (!databases[remoteDB]) databases[remoteDB] = new pouch(remoteDB);
+          if (!defaultDB) defaultDB = databases[remoteDB];
+          pouch.sync(databases[localDB], databases[remoteDB], {
+            live: true, retry: true
+          }).on('paused', function (err) {
+            // console.log('paused callback')
+          }).on('active', function () {
+            // console.log('active callback')
+          }).on('denied', function (err) {
+            vm.$pouch.errors[localDB] = error
+            vm.$pouch.errors = Object.assign({}, vm.$pouch.error)
+            // console.log('denied callback')
+          }).on('complete', function (info) {
+            // console.log('complete callback')
+          }).on('error', function (err) {
+            vm.$pouch.errors[localDB] = error
+            vm.$pouch.errors = Object.assign({}, vm.$pouch.error)
+            // console.log('error callback')
+          })
+          fetchSession(databases[remoteDB]);
+        },
+        session: {},
+        errors: {},
+        authError: null,
+        gotAuth: false
+      }
+      defineReactive(vm, '$pouch', $pouch);
+      
       var pouchOptions = this.$options.pouch;
       if (!pouchOptions) return;
       if (typeof pouchOptions == 'function') pouchOptions = pouchOptions();
-      var liveFinds = {};
       for (var key in pouchOptions) {
-        if (!pouchOptions.hasOwnProperty(key)) return;
+        if (!pouchOptions.hasOwnProperty(key)) continue;
         var pouchFn = pouchOptions[key]
         this.$data[key] = null;
         defineReactive(this, key, null);
@@ -42,7 +127,10 @@
             db = databaseParam;
           }
           else if (typeof databaseParam == 'string') {
-            if (!databases[databaseParam]) databases[databaseParam] = new pouch(databaseParam);
+            if (!databases[databaseParam]) {
+              databases[databaseParam] = new pouch(databaseParam);
+              if (vm.$pouch.gotAuth) login(databases[databaseParam]);
+            }
             db = databases[databaseParam];
           }
           if (!db) return;
@@ -63,18 +151,16 @@
           immediate: true
         })
       }   
-      
     }
   }
-
   
   var api = {
-    mixin: pouchdbvue,
+    mixin: vuePouch,
     install: function (Vue, options) {
       vue = Vue;
       pouch = options.pouch || PouchDB;
       defaultDB = options.defaultDB;
-      Vue.options = Vue.util.mergeOptions(Vue.options, pouchdbvue);
+      Vue.options = Vue.util.mergeOptions(Vue.options, vuePouch);
     }
   }
 
@@ -83,6 +169,6 @@
   } else if(typeof define === 'function' && define.amd) {
     define(function () { return api })
   } else if (typeof window !== 'undefined') {
-    window.PouchDBVue = api
+    window.VuePouch = api
   }
 })()
