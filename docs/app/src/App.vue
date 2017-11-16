700b1994959de5f912b6db44095c9f9cc11aa9f8
B Ricks <bricks@unomaha.edu>
2017-11-16 12:34:53 -0600

    Allow editing of tasks by adding empty options argument
    Line 9 does not properly update message and throws an error. This happens because PouchDB wants an object define the options as a third parameter. By adding an empty object, the error is removed and the tasks are properly updated.

--- docs/app/src/App.vue
+++ docs/app/src/App.vue
@@ -6,7 +6,7 @@
     <input v-model="message" placeholder="New Todo">
     <button @click="$pouch.post('todos', {message: message});message=''">Save Todo</button>
     <div v-for="todo in todos">
-      <input v-model="todo.message" @change="$pouch.put('todos', todo)">
+      <input v-model="todo.message" @change="$pouch.put('todos', todo, {})">
       <button @click="$pouch.remove('todos', todo)">Remove</button>
     </div>
   </div>
